5af3789460b7e5aa922d81a22e3a2530ae501a84
purnachand812 <50629828+purnachand812@users.noreply.github.com>
2023-02-17 13:30:55 +0530

    Update streamlyt_app.py

--- streamlyt_app.py
+++ streamlyt_app.py
@@ -20,8 +20,6 @@ def load_lottieurl(url):
 def local_css(file_name):
         with open(file_name) as f:
              st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
-local_css("style/style.css")
-
 
 
 lottie_coding= load_lottieurl("https://assets6.lottiefiles.com/packages/lf20_ngzwzxib.json")
@@ -80,6 +78,36 @@ with left_column:
     st.markdown(contact_form,unsafe_allow_html=True)
 with right_column:
     st.empty()
+
+    
+    
+    
+    /* Style inputs with type="text", select elements and textareas */
+input[type=text], input[type=email], textarea {
+  width: 100%; /* Full width */
+  padding: 12px; /* Some padding */ 
+  border: 1px solid #ccc; /* Gray border */
+  border-radius: 4px; /* Rounded borders */
+  box-sizing: border-box; /* Make sure that padding and width stays in place */
+  margin-top: 6px; /* Add a top margin */
+  margin-bottom: 16px; /* Bottom margin */
+  resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+}
+
+/* Style the submit button with a specific background color etc */
+input[type=submit] {
+  background-color: #04AA6D;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+/* When moving the mouse over the submit button, add a darker green color */
+input[type=submit]:hover {
+  background-color: #45a049;
+}
 
 
     
